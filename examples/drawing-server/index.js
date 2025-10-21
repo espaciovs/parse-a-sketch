1a69326c3e66b6f7f7de54395005d30e106374e4
ei <eivillamuelas@gmail.com>
2025-10-21 16:41:59 +0200

    a

---
+++ examples/drawing-server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  // Listen for the 'draw' event
+  socket.on('draw', (data) => {
+    // Receive drawing data and broadcast it to other clients
+    socket.broadcast.emit('draw', data);
+  });
+
+  // Listen for the 'draw' event
+  socket.on('consola', (log) => {
+    // Receive drawing data and broadcast it to other clients
+    console.log(log)
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
